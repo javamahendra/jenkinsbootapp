31cd089b7d43b389a5ace2415080499f27e921b2
MasterGST Tech sols <MasterGST Tech sols@LAPTOP-9O9IFBA0>
2021-05-25 10:44:39 +0530

    jenkins bootapp commit

---
+++ src/main/java/com/app/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		
+		return "Hello Jenkins spring boot app..."; 
+	}
+
+}
