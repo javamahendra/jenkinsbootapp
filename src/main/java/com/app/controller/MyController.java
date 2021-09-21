451060db094bb0d1f928074ec6d0031650e922db
javamahendra <javabymahendra@gmail.com>
2021-09-21 14:31:04 +0530

    Update MyController.java

--- src/main/java/com/app/controller/MyController.java
+++ src/main/java/com/app/controller/MyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-	
+	private static final String URL = "https://app.java-techsols.com";
 	@GetMapping(value = "/hello")
 	public String hello() {
 		//comments
